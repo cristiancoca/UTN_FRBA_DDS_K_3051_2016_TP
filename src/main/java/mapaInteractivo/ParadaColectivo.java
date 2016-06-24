1f6399bd6bdaaf9cb26b2f41c73b32ad6d40d50e
cristiancoca <omarcuervo24@gmail.com>
2016-06-24 18:33:47 -0300

    paradacolectivo
    aca esta lo q seria el poi paradacolectivo

--- src/main/java/mapaInteractivo/ParadaColectivo.java
+++ src/main/java/mapaInteractivo/ParadaColectivo.java
@@ -1,5 +1,51 @@
 package mapaInteractivo;
 
 public class ParadaColectivo extends TipoPOI {
+	
+		/*****Atributos*****/
+		private String lineacolectivo;
 
-}
+		/*****para q este a menos de una cuadra tiene q estar en la misma calle*****/ 
+		 /***** y a una diferencia de altura menor q 100*****/
+
+		public void setnumerocolectivo(String numcolectivo){
+			this.lineacolectivo=numcolectivo;
+		}
+		public String getnumerocolectivo (){
+			return this.lineacolectivo;
+		}
+		public boolean estasCercaDe(Ubicacion unaubicacion , Ubicacion miUbicacion ){
+			   String calle1=unaubicacion.getCalle();
+			   String calle2=miUbicacion.getCalle();
+			   int altura1= unaubicacion.getAltura();
+			   int altura2=miUbicacion.getAltura();
+			   
+			   int difaltura = Math.abs(altura1 - altura2);
+			 
+				if (calle1==calle2 && difaltura < 100){
+				 return true;
+				 }
+				 else {
+					 return false;
+			 
+		 }
+		}
+		public boolean estasDisponible(){
+			return true ;
+		}
+		public boolean esElPuntoBuscado( String textolibre){
+		
+			String numerolinea =  this.getnumerocolectivo();
+			if (textolibre== numerolinea){
+				return true;
+			}else {
+				return false;
+			}
+		}
+	}
+
+
+
+
+
+
